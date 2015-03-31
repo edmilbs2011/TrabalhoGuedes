18b8ed5dbe74e62e23fd620dc59c7775079f0450
abraaoalves <abraao.teodosio@gmail.com>
2015-03-31 15:40:23 -0300

    fix test file to status red

--- TrabalhoGuedes/main-test.c
+++ TrabalhoGuedes/main-test.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define FAIL() printf("\nfailure in %s() line %d\n", __func__, __LINE__)
-#define _assert(test) do { if (!(test)) { FAIL(); return 1; } } while(0)
-#define _verify(test) do { int r=test(); tests_run++; if(r) return r; } while(0)
-
 int tests_run = 0;
-int main() {
-	int result = all_tests();
-	if (result == 0)
-		printf("PASSED\n");
-	printf("Tests run: %d\n", tests_run);
 
-	return result != 0;
-}
-int all_tests() {
-	_verify(first_test);
-	return 0;
-}
-
+void FAIL() { printf("\nfailure in %s() line %d\n", __FUNCTION__, __LINE__); }
+int _assert(test) { do { if (!(test)) { FAIL(); return 1; } } while (0); }
+int _verify(int(*test)()) { do { int r = test(); tests_run++; if (r) return r; } while (0); }
 
 //// My tests here
 
-static int first_test() {
+int firstTest() {
 	_assert(false);
 	return 0;
 }
+
+//// Finish my tests
+
+int all_tests() {
+	_verify(firstTest);
+	return 0;
+}
+
+int main() {
+
+		int result = all_tests();
+	if (result == 0)
+		printf("PASSED\n");
+	printf("Tests run: %d\n", tests_run);
+	
+	return getchar(), result != 0;
+}
+
+
 
